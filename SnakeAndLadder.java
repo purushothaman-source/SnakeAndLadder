b0f3c74220e4481de6b209bd861e18ba7c8d8b1b
purushothaman-source <purushoth99jun@gmail.com>
2020-12-05 11:15:06 +0530

    REFRACTORED : removed extra spaces

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,11 +2,11 @@ package com.program.snakeandladder;
 class SnakeAndLadderGame{
 	//Initializing variable playerA and rollDie
 	int playerA=0;
-	int rollDie;	
+	int rollDie;
 }
 //MainClass
 public class SnakeAndLadder {
  //mainMethod
-	public static void main(String[] args) {    
+	public static void main(String[] args) {
 	}
 }
